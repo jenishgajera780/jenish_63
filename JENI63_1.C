55eb3b700babeab3790fc9607917b81383239b59
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-06-30 17:52:04 +0530

    Add files via upload

---
+++ JENI63_1.C
@@ -0,0 +1,15 @@
+// Q1 writea program to ptint the sum of all numbers from 1 to n using for loop.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int j,e,n,i=0;
+clrscr();
+printf("ENTER ANY NUMBER");
+scanf("%d",&j):
+
+
+
+getch();
+}
