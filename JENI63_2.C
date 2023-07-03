ce3fe736f720ecfd722c5ad5a6c7e698488da043
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-03 11:35:44 +0530

    Update JENI63_2.C

--- JENI63_2.C
+++ JENI63_2.C
@@ -1,2 +1,23 @@
-//
-#
+
+// Q2 write a programe to print the factorial of number n using for loop.
+
+
+#include<stdio.h>
+#include<stdio.h>
+void main(){
+int j,e,n,i = 1;
+
+clrscr();
+
+printf("enter any number: ");
+scanf("%d",&j);
+
+for(e = 1;e<=j;e++)
+{
+    n=e*i;
+    i=n;
+}
+printf("the factorial is %d",i);
+
+    getch();
+}
